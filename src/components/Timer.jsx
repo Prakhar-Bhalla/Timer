eaa3468f5a6c769573d2cd5947aca7c1b9d3215a
Prakhar-Bhalla <prakharbhalla2016@gmail.com>
2022-01-15 01:39:42 +0530

    Update Timer.jsx

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -4,6 +4,7 @@ export const Timer = () => {
     const [clockSec, setClockSec] = useState("");
     const [timeSpan, setTimeSpan] = useState({});
     const [flag, setFlag] = useState(false);
+    const [isWrong, setIsWrong] = useState(false);
 
     const setTime = (event) => {
         const {name, value} = event.target; 
@@ -15,6 +16,12 @@ export const Timer = () => {
     
     const startClock = () => {
         setFlag(false);
+        setIsWrong(false);
+        if(timeSpan.Start > timeSpan.End)
+        {
+            setClockSec("");
+            return setIsWrong(true);
+        }
         setClockSec(timeSpan.Start)
         let id = setInterval(() => {
             setClockSec(p => {
@@ -28,7 +35,7 @@ export const Timer = () => {
         }, 1000)
     }
 
-    return <div style={{margin : "20px 0"}}>{flag && <p style={{color : "red"}}>Time up!</p>}Counter : <input style={{width : "20px", textAlign : "center", outline : "none", border : "2px solid brown"}} readOnly="readOnly" value={clockSec}/>
+    return <div style={{margin : "20px 0"}}>{flag && <p style={{color : "red"}}>Time up!</p>}{isWrong && <p style={{color : "red"}}>Wrong input!</p>}Counter : <input style={{width : "20px", textAlign : "center", outline : "none", border : "2px solid brown"}} readOnly="readOnly" value={clockSec}/>
         <div style={{margin : "15px 0"}}>
         <input placeholder="Start time" onChange={setTime} style={{width : "65px", textAlign : "center", outline : "none", border : "2px solid green", margin : "0 5px"}} name="Start"/>
         <input placeholder="End time" onChange={setTime} style={{width : "65px", textAlign : "center", outline : "none", border : "2px solid red", margin : "0 5px"}} name="End"/>
